73d1a7ecdb453068c3d9704b8656f6fc5784a405
Juan Carlos Villasenor Derbez <juancarlos@ucsb.edu>
2022-05-26 16:13:46 -0700

    Updated SCC_t

---
+++ scripts/project_scc_t.R
@@ -0,0 +1,27 @@
+######################################################
+#title#
+######################################################
+# 
+# Purpose
+#
+######################################################
+
+
+library(here)
+library(janitor)
+library(tidyverse)
+
+source(here("scripts", "_functions.R"))
+
+scc <- read_csv(here("data", "raw", "tsd_2021_annual_unrounded.csv")) %>% 
+  clean_names() %>% 
+  mutate(time = year - 2022) %>% 
+  select(year, time, scc_t = x2_5_percent_co2)
+
+mod <- lm(scc_t ~ time, data = scc)
+
+scc_2070 <- tibble(time = 0:nsteps) %>% 
+  mutate(scc_pred_t = predict(mod, newdata = .))
+
+saveRDS(object = scc_2070,
+        file = here("data", "processed", "scc_pred.rds"))
